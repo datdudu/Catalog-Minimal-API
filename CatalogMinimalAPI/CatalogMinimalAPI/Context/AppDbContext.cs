dd9fa997de89d47e4386b10643652afed0b594be
Eduardo Carvalho Cardoso <dxcar10000@hotmail.com>
2023-07-10 19:47:16 -0300

    --COMMIT Initializing Migrations with EF Core

---
+++ CatalogMinimalAPI/CatalogMinimalAPI/Context/AppDbContext.cs
@@ -0,0 +1,50 @@
+﻿using CatalogMinimalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogMinimalAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { }
+
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //Categories
+            mb.Entity<Category>().HasKey(c => c.CategoryId);
+
+            mb.Entity<Category>().Property(c => c.Name)
+                                 .HasMaxLength(100)
+                                 .IsRequired();
+
+            mb.Entity<Category>().Property(c => c.Description)
+                                 .HasMaxLength(150)
+                                 .IsRequired();
+
+            //Products
+            mb.Entity<Product>().HasKey(c => c.ProductId);
+
+            mb.Entity<Product>().Property(p => p.Name)
+                                .HasMaxLength(100)
+                                .IsRequired();
+
+            mb.Entity<Product>().Property(p => p.Description)
+                                .HasMaxLength(150);
+
+            mb.Entity<Product>().Property(p => p.Image)
+                                .HasMaxLength(100);
+
+            mb.Entity<Product>().Property(p => p.Price)
+                                .HasPrecision(14, 2);
+
+            //Relationship
+            mb.Entity<Product>()
+                .HasOne<Category>(c => c.Category)
+                    .WithMany(p => p.Products)
+                        .HasForeignKey(c => c.CategoryId);
+        }
+    }
+}
